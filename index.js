cf868ab1158d2b568e459fbb7ab3374fc5c3f176
Liliia <liliia.shvorob@oa.edu.ua>
2023-03-12 15:55:44 +0100

    updated route in index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/api/Petshop", petsRouter);
+app.use("/api/pets", petsRouter); // updated route
 
 connectDB().then(() => {
   app.listen(PORT, () => {
